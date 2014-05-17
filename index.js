6ddd9a9d1ea7bf76f7e772a8942b6dffc15055bf
Carlos Rodriguez <carlos@s8f.org>
2014-05-17 12:44:41 -0700

    optional tag

--- index.js
+++ index.js
@@ -2,18 +2,18 @@ exports.decode = function (pem) {
   return Buffer(
     pem
       .toString('ascii')
-      .replace(/.*\-\-\-\-\-BEGIN .*\-\-\-\-\-\s*/, '')
-      .replace(/\r?\n\-\-\-\-\-END .*\-\-\-\-\-.*/, '')
+      .replace(/.*\-\-\-\-\-BEGIN.*\-\-\-\-\-\s*/, '')
+      .replace(/\r?\n\-\-\-\-\-END.*\-\-\-\-\-.*/, '')
       .replace(/\s*/g, '')
     , 'base64');
 };
 
 exports.encode = function (buf, tag) {
   return (
-    '-----BEGIN ' + tag + '-----\n'
+    '-----BEGIN' + (tag ? ' ' + tag : '') + '-----\n'
     + Buffer(buf)
       .toString('base64')
       .match(/.{1,64}/g)
       .join('\n')
-    + '\n-----END ' + tag + '-----');
+    + '\n-----END' + (tag ? ' ' + tag : '') + '-----');
 };
