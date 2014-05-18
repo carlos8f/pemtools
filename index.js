d106dd6d2a846fe0236c3e407560a3eccec56872
Carlos Rodriguez <carlos@s8f.org>
2014-05-17 21:40:09 -0700

    overhaul

--- index.js
+++ index.js
@@ -1,13 +1,117 @@
-var crypto = require('crypto');
+var crypto = require('crypto')
+  , sshKeyToPEM = require('ssh-key-to-pem')
 
-exports.decode = function (pem, passphrase) {
-  pem = pem.toString('ascii');
-  var dekInfo = pem.match(/DEK-Info: ([^,]+),([0-9A-F]+)/i);
+// constructor
+module.exports = exports = function (input, tag, passphrase) {
+  return new PEM(input, tag, passphrase);
+};
+
+exports.serialize = function (buffers) {
+  var parts = []
+    , idx = 0
+  buffers.forEach(function (part) {
+    var len = Buffer(4);
+    if (typeof part === 'string') part = Buffer(part);
+    len.writeUInt32BE(part.length, 0);
+    parts.push(len);
+    idx += len.length;
+    parts.push(part);
+    idx += part.length;
+  });
+  return Buffer.concat(parts);
+};
+
+exports.unserialize = function (buf) {
+  var parts = [];
+  var l = buf.length, idx = 0;
+  while (idx < l) {
+    var dlen = buf.readUInt32BE(idx);
+    idx += 4;
+    var start = idx;
+    var end = start + dlen;
+    var part = buf.slice(start, end);
+    parts.push(part);
+    idx += part.length;
+  }
+  return parts;
+};
+
+exports.writeSSHPubkey = function (opts) {
+  opts || (opts = {});
+  return (opts.type || 'ssh-rsa')
+    + ' '
+    + (exports.serialize([
+      opts.type || 'ssh-rsa',
+      opts.exponent,
+      opts.modulus
+    ]).toString('base64'))
+    + ' '
+    + opts.comment
+    + '\n';
+};
+
+exports.readSSHPubkey = function (str) {
+  var input = str.trim().split(' ');
+  var buf = Buffer(input[1].trim(), 'base64');
+  var parts = exports.unserialize(buf);
+  if (parts.length != 3) throw new Error('invalid pubkey field count: ' + parts.length);
+  return {
+    type: parts[0].toString('ascii'),
+    modulus: parts[2],
+    exponent: parts[1],
+    bits: (parts[2].length - 1) * 8,
+    comment: input[2] ? input.slice(2).join(' ') : ''
+  };
+};
+
+function PEM (input, tag, passphrase) {
+  if (Buffer.isBuffer(input)) {
+    this.encode(input, tag, passphrase);
+  }
+  else if (input) {
+    input = input.toString('ascii').trim();
+    if (input.indexOf('ssh-') === 0) {
+      this.pubkey = exports.readSSHPubkey(input);
+      this.sshPubkey = input;
+      input = sshKeyToPEM(input);
+    }
+    else if (input.indexOf('-----BEGIN PUBLIC KEY-----') === 0) {
+      this.sshPubkey = sshKeyToPEM.pemToRsaSSHKey(input);
+      this.pubkey = exports.readSSHPubkey(this.sshPubkey);
+    }
+    this.decode(input, passphrase);
+  }
+}
+
+PEM.prototype.toString = function () {
+  return this.pem;
+};
+
+PEM.prototype.toBuffer = function () {
+  return this.buf;
+};
+
+PEM.prototype.toSSH = function () {
+  if (!this.sshPubkey) throw new Error('not a pubkey!');
+  return this.sshPubkey;
+};
+
+// decode PEM string -> buffer
+PEM.prototype.decode = function (str, passphrase) {
+  // store input
+  if (str) this.pem = str.toString('ascii').trim();
+  var pem = this.pem;
+  var match = this.pem.match(/\-\-\-\-\-\s*BEGIN\s*([^\-]*)\-\-\-\-\-\r?\n/);
+  if (!match) throw new Error('no start tag found in PEM');
+  this.tag = match[1].trim();
+  // parse DEK-Info
+  var dekInfo = this.pem.match(/DEK-Info: ([^,]+),([0-9A-F]+)/i);
   if (dekInfo) {
-    if (typeof passphrase === 'undefined')
+    if (typeof passphrase !== 'string')
       throw new Error('PEM is encrypted but no passphrase given');
     var algorithm = dekInfo[1].toLowerCase();
     var salt = Buffer(dekInfo[2], 'hex');
+    // @todo: is this necessary?
     var keyLength = (function (algorithm) {
       switch (algorithm) {
         case 'aes-128-cbc': return 128;
@@ -31,69 +135,51 @@ exports.decode = function (pem, passphrase) {
       .replace(/Proc-Type: .*\s*/, '')
       .replace(/DEK-Info: .*\s*/, '');
   }
-  var buf = Buffer(
+
+  // strip to base64 data
+  this.buf = Buffer(
     pem
       .toString('ascii')
       .replace(/.*\-\-\-\-\-BEGIN.*\-\-\-\-\-\s*/, '')
       .replace(/\r?\n\-\-\-\-\-END.*\-\-\-\-\-.*/, '')
       .replace(/\s*/g, '')
     , 'base64');
+
+  // decrypt if needed
   if (dekInfo) {
-    var buf1 = decipher.update(buf);
-    buf = Buffer.concat([buf1, decipher.final()]);
+    var buf1 = decipher.update(this.buf);
+    this.buf = Buffer.concat([buf1, decipher.final()]);
   }
-  return buf;
+  return this.buf;
 };
 
-/**
- * Options:
- *
- * - passphrase: encrypt PEM using passphrase
- * - algorithm: cipher algorithm to use (default: aes-256-cbc)
- * - tag: identifying string for boundary, i.e. "PRIVATE KEY"
- */
-exports.encode = function (buf, options) {
-  options || (options = {});
+// encode buffer -> PEM string
+PEM.prototype.encode = function (buf, tag, passphrase) {
+  if (buf) this.buf = buf;
+  if (tag) this.tag = tag;
   var dekInfo = '';
-  if (options.passphrase) {
-    var passphrase = options.passphrase.toString();
-    var algorithm = options.algorithm
-      ? options.algorithm.toLowerCase()
-      : 'aes-256-cbc';
+  if (passphrase) {
+    var passphrase = passphrase.toString();
+    var algorithm = 'aes-256-cbc';
     var salt = crypto.randomBytes(8);
     var dekInfo = ('Proc-Type: 4,ENCRYPTED\nDEK-Info: '
       + (algorithm.toUpperCase())
       + ','
       + (salt.toString('hex').toUpperCase())
       + '\n\n');
-    var keyLength = (function (algorithm) {
-      switch (algorithm) {
-        case 'aes-128-cbc': return 128;
-        case 'aes-192-cbc': return 192;
-        case 'aes-256-cbc': return 256;
-        case 'bf-cbc': return 256;
-        case 'cast-cbc': return 128;
-        case 'des-cbc': return 64;
-        case 'desx-cbc': return 184;
-        case 'des-ede-cbc': return 128;
-        case 'des-ede3-cbc': return 192;
-        case 'idea-cbc': return 128;
-        case 'rc2-cbc': return 256;
-        case 'rc5-cbc': return 128;
-        default: return 512;
-      }
-    })(algorithm);
+    var keyLength = 256;
     var key = crypto.pbkdf2Sync(passphrase, salt, 1, keyLength);
     var cipher = crypto.createCipher(algorithm, key, salt);
     var buf1 = cipher.update(buf);
     buf = Buffer.concat([buf1, cipher.final()]);
   }
-  return (
-    '-----BEGIN' + (options.tag ? ' ' + options.tag : '') + '-----\n'
+  this.pem = (
+    '-----BEGIN' + (tag ? ' ' + tag : '') + '-----\n'
     + dekInfo
     + Buffer(buf)
       .toString('base64')
       .match(/.{1,64}/g)
       .join('\n')
-    + '\n-----END' + (options.tag ? ' ' + options.tag : '') + '-----');
+    + '\n-----END' + (tag ? ' ' + tag : '') + '-----');
+  return this.pem;
 };
--- test.js
+++ test.js
@@ -1,10 +1,11 @@
-codec = require('./');
+pemtools = require('./');
 assert = require('assert');
 fs = require('fs');
 path = require('path');
 crypto = require('crypto');
 rimraf = require('rimraf');
 request = require('request');
+prompt = require('cli-prompt');
  
 tmpDir = path.join(require('os').tmpDir(), require('idgen')());
  
@@ -38,27 +39,42 @@ describe('tests', function () {
   });
 
   it('convert to pem', function () {
-    pem = codec.encode(buf, {tag: 'COOL IMAGE'});
-    assert.equal(crypto.createHash('sha1').update(pem).digest('hex'), 'dd8c857178055695f1da6f624f513464e5178af2');
+    pem = pemtools(buf, 'COOL IMAGE');
+    assert.equal(crypto.createHash('sha1').update(pem.toString()).digest('hex'), 'dd8c857178055695f1da6f624f513464e5178af2');
   });
 
   it('convert to buffer', function () {
-    var back = codec.decode(pem);
-    assert.deepEqual(back, buf);
-    assert.equal(crypto.createHash('sha1').update(back).digest('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
+    var back = pemtools(pem.toString());
+    assert.deepEqual(back.toBuffer(), buf);
+    assert.equal(crypto.createHash('sha1').update(back.toBuffer()).digest('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
   });
 
   it('converts to encrypted pem', function () {
-    pem = codec.encode(buf, {passphrase: 'totally secret', tag: 'COOL SECRET IMAGE'});
-    assert(pem.match(/DEK-Info: /));
+    pem = pemtools(buf, 'COOL SECRET IMAGE', 'totally secret');
+    assert(pem.toString().match(/DEK-Info: /));
   });
 
   it('decrypts', function () {
     assert.throws(function () {
-      codec.decode(pem, 'totally awesome');
+      pemtools(pem, null, 'totally awesome');
     });
-    var back = codec.decode(pem, 'totally secret');
-    assert.deepEqual(back, buf);
-    assert.equal(crypto.createHash('sha1').update(back).digest('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
+    var back = pemtools(pem.toString(), 'COOL SECRET IMAGE', 'totally secret');
+    assert.deepEqual(back.toBuffer(), buf);
+    assert.equal(crypto.createHash('sha1').update(back.toBuffer()).digest('hex'), '2bce2ffc40e0d90afe577a76db5db4290c48ddf4');
+  });
+});
+
+describe('ssh key', function () {
+  it('public key', function (done) {
+    fs.readFile('test.pub', {encoding: 'ascii'}, function (err, pubkey) {
+      assert.ifError(err);
+      pem = pemtools(pubkey);
+      assert.equal(pem.pubkey.bits, 2048);
+      assert.equal(pem.pubkey.modulus.toString('hex'), '00cf9f910fbd0d19cf793f6e48e6caba1dd472bce658d41d73ecd4703649c133f377e9c5338283b6d2baa51789333f5c8a61574b53993311a581248449c2407156c2d893f7d557a3d71fa2b454c1e482562d4588dc73e40d2a168d3c49af3b7bc2004f99a62fc9e6abd421ed6da8d8aeb44c753698a51cfaf90c6f2d780d2468faae1b88c121367220360a05c76a0e7ff66118a6043ba53d3413b33ee973335419da8cb4408a9a04ec55bae155dfda7fdaa9d6db0fc1e054a688dc4b5fccb671fb388946ea5488d57faf727b64dfc28a530e607a1b6f0949bb7f44379dda5908dad53395d2a2f0f6e744c3b8b827e1a1f2deacce82d2f5873ef1cdc2c1ca162611');
+      assert.equal(pem.pubkey.exponent.toString('hex'), '010001');
+      var encoded = pemtools.writeSSHPubkey(pem.pubkey);
+      assert.deepEqual(encoded, pubkey);
+      done();
+    });
   });
 });
