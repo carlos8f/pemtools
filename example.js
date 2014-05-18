b951feab54bb8a7bd98551f6a6d6fd7f8d5f6c9e
Carlos Rodriguez <carlos@s8f.org>
2014-05-18 13:55:14 -0700

    no need for separate private key for example

--- example.js
+++ example.js
@@ -1,5 +1,5 @@
 var pemtools = require('./');
 var fs = require('fs');
-var str = fs.readFileSync('example.pem', {encoding: 'ascii'});
-var pem = pemtools(str, null, '1234');
+var str = fs.readFileSync('test.pem', {encoding: 'ascii'});
+var pem = pemtools(str, null, 'this is so super cool');
 console.log(pem.privateKey);
