8b15edf2738c0594c9ac66cd33a1ee2d47a62e67
Carlos Rodriguez <carlos@s8f.org>
2014-05-18 20:37:30 -0700

    remove example

--- example.js
+++
@@ -1,5 +0,0 @@
-var pemtools = require('./');
-var fs = require('fs');
-var str = fs.readFileSync('test.pem', {encoding: 'ascii'});
-var pem = pemtools(str, null, 'this is so super cool');
-console.log(pem.privateKey);
